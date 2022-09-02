08d0a9830cf3e23ac8062de33d34cc9dad0f31da
tristeoar <yuhangoar@163.com>
2022-09-02 22:27:24 +0800

    delete LR Char finish

--- oar/utils/strutil.h
+++ oar/utils/strutil.h
@@ -8,8 +8,8 @@ namespace oar {
 
   namespace strutil {
     
-    
-    void _strSplit(std::string &str, std::vector<std::string> &v, std::string spacer) {
+    // 要加inline否则会有multiple definition的问题
+    inline void _strSplit(std::string &str, std::vector<std::string> &v, std::string spacer) {
       int pos1, pos2;
       int len = spacer.length(); //记录分隔符的长度
       pos1 = 0;
@@ -24,32 +24,35 @@ namespace oar {
       if (pos1 != str.length()) //分割最后一个部分
 	v.push_back(str.substr(pos1));  
     }
-
-
-    
-    //    template<typename T = std::string>
-    void strSplit(std::string &str, std::vector<std::string> &v, std::string spacer)
+								     
+    // inline
+    template<typename T = std::string>
+    inline void strSplit(std::string str, std::vector<std::string> &v, T spacer)
     {
       _strSplit(str,v,spacer);
     }
 
-    //    template<>
-    void strSplit(std::string &str, std::vector<std::string> &v, const char* spacer)
+    template<>
+    inline void strSplit(std::string str, std::vector<std::string> &v, const char* spacer)
     {
       _strSplit(str,v,std::string(spacer));
     }
+											   
     
-    
-    //    template<>
-    void strSplit(std::string &str, std::vector<std::string> &v, char spacer)
+    template<>
+    inline void strSplit(std::string str, std::vector<std::string> &v, char spacer)
     {
       _strSplit(str,v,std::string(1,spacer));
     }
     
 
+    void deleteLeftChar(std::string &str,const char c);
+    void deleteLeftChar(char *str,const char c);
 
+    void deleteRightChar(std::string &str,const char c);
+    void deleteRightChar(char *str,const char c);
     void test (int i) ;
-    
+
     
   } // namespace strutil
 } // namespace oar
--- test/strutilTest.cc
+++ test/strutilTest.cc
@@ -1,19 +1,33 @@
 #include "../oar/utils/strutil.h"
 #include <iostream>
-
-//using namespace std;
+#include <string.h>
+using namespace std;
 using namespace oar;
 
 int main() {
-  //  vector<string> vec;
+  vector<string> vec;
   
-  //strutil::strSplit("1231232 213 12321321 3",vec,string(" "));
-  //  strutil::strSplit("1231232 213 12321321 3",vec,' ');
-  /*  
-      for(auto &s:vec) {
-      cout << s << endl;
-      }
-  */
-  strutil::test(10);
+  strutil::strSplit("1231232 213 12321321 3",vec,string(" "));
+  //strutil::strSplit("1231232 213 12321321 3",vec,' ');
+    
+  for(auto &s:vec) {
+    cout << s << endl;
+  }
+      
+  
+  char str[32] = "  begin..!@.ab  cd..$%.end  ";
+  printf("sizeof str is %d\n",strlen(str));
+  string s(str);
+  strutil::deleteLeftChar(s,' ');
+  strutil::deleteLeftChar(str, ' ');
+  cout << "after deleteleftchar string s is |" << s << "|" <<endl;
+  cout << "after deleteleftchar char* str is |" << str << "|" <<endl;
+
+  strutil::deleteRightChar(s,' ');
+  strutil::deleteRightChar(str, ' ');
+  cout << "after deleterightchar string s is |" << s << "|" <<endl;
+  cout << "after deleterightchar char* str is |" << str << "|" <<endl;
+
+
   return 0;
 }
